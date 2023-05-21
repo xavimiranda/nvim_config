7f531756a746cfe5481807f3eac2145016ef056d
xavier.miranda <xavimiranda@gmail.com>
2023-05-22 00:07:10 +0100

    Add nvim-tree

--- lua/xav1t/packer.lua
+++ lua/xav1t/packer.lua
@@ -2,5 +2,6 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'nvim-tree/nvim-web-devicons'
 	use 'folke/tokyonight.nvim'
+	use 'nvim-tree/nvim-tree.lua'
 	use 'nvim-lualine/lualine.nvim'
 end)
--- lua/xav1t/plugin_config/init.lua
+++ lua/xav1t/plugin_config/init.lua
@@ -1,2 +1,3 @@
 require("xav1t.plugin_config.tokyonight")
 require("xav1t.plugin_config.lualine")
+require("xav1t.plugin_config.nvim-tree")
