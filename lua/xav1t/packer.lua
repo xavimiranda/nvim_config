bc0625f2a625f8cbfc46ccbcedd4db3bc528bd33
xavier.miranda <xavimiranda@gmail.com>
2023-05-22 23:09:39 +0100

    Add nvim-cmp

--- lua/xav1t/packer.lua
+++ lua/xav1t/packer.lua
@@ -8,10 +8,24 @@ return require('packer').startup(function(use)
 
         use 'nvim-treesitter/nvim-treesitter'
 	use {
-		'nvim-telescope/telescope.nvim', 
+		'nvim-telescope/telescope.nvim',
                 tag = '0.1.1',
 		requires = { {'nvim-lua/plenary.nvim'} }
-	}	
+	}
+
+        --completions
+        use 'hrsh7th/nvim-cmp'
+        use 'hrsh7th/cmp-nvim-lsp'
+        use 'hrsh7th/cmp-buffer'
+        use 'hrsh7th/cmp-buffer'
+        use 'hrsh7th/cmp-cmdline'
+        use 'hrsh7th/cmp-path'
+        use {
+            'L3MON4D3/LuaSnip',
+            dependencies = { 'rafamadriz/friendly-snippets'}
+        }
+        use 'saadparwaiz1/cmp_luasnip'
+        use 'rafamadriz/friendly-snippets'
 
         --lsp plugins
         use {
--- lua/xav1t/plugin_config/lsp_config.lua
+++ lua/xav1t/plugin_config/lsp_config.lua
@@ -14,6 +14,9 @@ local on_attach = function(_, _)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 require('lspconfig').lua_ls.setup{
-    on_attach = on_attach
+    on_attach = on_attach,
+    capabilities = capabilities,
 }
